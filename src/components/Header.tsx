4486d01474e6e79b3994b06d5efe973cda227d73
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-06-13 17:10:14 +0000

    ssssssssssssssss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Text, Button, Flex, Image, Spacer, extendTheme, ChakraProvider } from '@chakra-ui/react';
+import { Box, Text, Button, Flex, Image, extendTheme, ChakraProvider, Center } from '@chakra-ui/react';
 
 const theme = extendTheme({
   colors: {
@@ -12,30 +12,32 @@ const Header: React.FC = () => {
   return (
     <ChakraProvider theme={theme}>
       <Box bg="navy" paddingY="2" paddingX="4">
-        <Flex align="center">
-          {/* Logo Section */}
-          <Image src="/agaviclogo.png" alt="Logo" height="30px" />
-          
-          {/* Text Section */}
-          <Text color="white" fontSize="lg" ml="4">
-            AGAVIC
-          </Text>
+        <Center>
+          <Flex width="full" maxWidth="1200px" align="center" justify="space-between">
+            <Flex align="center">
+              {/* Logo Section */}
+              <Image src="/agaviclogo.png" alt="Logo" height="30px" />
+              
+              {/* Text Section */}
+              <Text color="white" fontSize="lg" ml="2">
+                AGAVIC
+              </Text>
+            </Flex>
 
-          <Spacer />
-
-          {/* Button Section */}
-          <Button
-            variant="outline"
-            color="white"
-            borderColor="white"
-            _hover={{ bg: 'white', color: 'navy' }}
-            borderRadius="8px"
-            paddingX="6"
-            paddingY="4"
-          >
-            Une question ?
-          </Button>
-        </Flex>
+            {/* Button Section */}
+            <Button
+              variant="outline"
+              color="white"
+              borderColor="white"
+              _hover={{ bg: 'white', color: 'navy' }}
+              borderRadius="8px"
+              paddingX="6"
+              paddingY="4"
+            >
+              Une question ?
+            </Button>
+          </Flex>
+        </Center>
       </Box>
     </ChakraProvider>
   );
