92fe6e8449e2d4cdfb014f91a07952bd4724c947
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-06-20 20:13:46 +0000

    fvdfgvfddf

--- src/recommandation/modal/ProjectModification.tsx
+++ src/recommandation/modal/ProjectModification.tsx
@@ -11,6 +11,11 @@ import {
   ModalHeader,
   ModalBody,
   ModalCloseButton,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
 } from '@chakra-ui/react';
 import { supabase } from './../../supabaseClient'; // Importez votre client Supabase
 import { useUuid } from './../../context/UuidContext';
@@ -19,7 +24,8 @@ const ProjectModification: React.FC<{ isOpen: boolean, onClose: () => void }> = 
   const [initialPayment, setInitialPayment] = useState('');
   const [monthlyPayment, setMonthlyPayment] = useState('');
   const [investmentHorizon, setInvestmentHorizon] = useState('');
-  
+  const [showAlert, setShowAlert] = useState(false);
+
   const { uuid } = useUuid();
 
   useEffect(() => {
@@ -57,52 +63,68 @@ const ProjectModification: React.FC<{ isOpen: boolean, onClose: () => void }> = 
     if (error) {
       console.error('Error updating project data:', error);
     } else {
-      onClose();
+      setShowAlert(true);
     }
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="lg">
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Modification de votre projet</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <VStack spacing={4} align="stretch">
-            <Box>
-              <Text fontWeight="bold">Versement initial</Text>
-              <Text color="gray.500" mb={2}>Il s'agit du 1er versement réalisé sur votre contrat Yomoni.</Text>
-              <Input
-                value={initialPayment}
-                onChange={(e) => setInitialPayment(e.target.value)}
-                size="md"
-              />
-            </Box>
-            <Box>
-              <Text fontWeight="bold">Versement mensuel</Text>
-              <Text color="gray.500" mb={2}>55% de nos clients placent en moyenne 200 € / mois.</Text>
-              <Input
-                value={monthlyPayment}
-                onChange={(e) => setMonthlyPayment(e.target.value)}
-                size="md"
-              />
-            </Box>
-            <Box>
-              <Text fontWeight="bold">Horizon de placement</Text>
-              <Text color="gray.500" mb={2}>Votre investissement reste disponible à tout moment.</Text>
-              <Input
-                value={investmentHorizon}
-                onChange={(e) => setInvestmentHorizon(e.target.value)}
-                size="md"
-              />
-            </Box>
-          </VStack>
-          <Button colorScheme="blue" mt={6} w="full" onClick={handleSave}>
-            Valider
-          </Button>
-        </ModalBody>
-      </ModalContent>
-    </Modal>
+    <>
+
+      <Modal isOpen={isOpen} onClose={onClose} size="lg">
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalHeader>Modification de votre projet</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={4} align="stretch">
+              <Box>
+                <Text fontWeight="bold">Versement initial</Text>
+                <Text color="gray.500" mb={2}>Il s'agit du 1er versement réalisé sur votre contrat Yomoni.</Text>
+                <Input
+                  value={initialPayment}
+                  onChange={(e) => setInitialPayment(e.target.value)}
+                  size="md"
+                />
+              </Box>
+              <Box>
+                <Text fontWeight="bold">Versement mensuel</Text>
+                <Text color="gray.500" mb={2}>55% de nos clients placent en moyenne 200 € / mois.</Text>
+                <Input
+                  value={monthlyPayment}
+                  onChange={(e) => setMonthlyPayment(e.target.value)}
+                  size="md"
+                />
+              </Box>
+              <Box>
+                <Text fontWeight="bold">Horizon de placement</Text>
+                <Text color="gray.500" mb={2}>Votre investissement reste disponible à tout moment.</Text>
+                <Input
+                  value={investmentHorizon}
+                  onChange={(e) => setInvestmentHorizon(e.target.value)}
+                  size="md"
+                />
+              </Box>
+            </VStack>
+            <Button colorScheme="blue" mt={6} w="full" onClick={handleSave}>
+              Valider
+            </Button>
+            {showAlert && (
+              <Alert status="success">
+                <AlertIcon />
+                <Box flex="1">
+                  <AlertTitle>Mise à jour réussie!</AlertTitle>
+                  <AlertDescription>
+                    Les valeurs ont été mises à jour avec succès.
+                  </AlertDescription>
+                </Box>
+                <CloseButton position="absolute" right="8px" top="8px" onClick={() => setShowAlert(false)} />
+              </Alert>
+            )}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
